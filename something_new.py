d26b6dc4ec87fe50fceded112f986cd17073f30d
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-07-12 19:56:35 +0900

    Create something_new.py

---
+++ something_new.py
@@ -0,0 +1,10 @@
+answer == 0
+def in_for(num, inp):
+    for each in inp:
+        if each == num:
+            return True
+            break
+if in_for(5, [5,3,4,2,1,3,4,2,9]) == True:
+    answer += 1
+def in_bin(num, inp):
+    return True
