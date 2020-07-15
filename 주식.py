732ad09a84d3c75009b229929162126b5e653ad4
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-07-15 21:51:29 +0900

    Update 주식.py

--- 주식.py
+++ 주식.py
@@ -2,15 +2,14 @@ def solution(prices):
     length = 0
     someL = []
     answer = []
-    for each in range(1, len(prices)):
-        for eachAns in range(each, len(prices)-each):
-            print(len(prices)-each)
-            if prices[each-1] > prices[eachAns]:
+    for i in range(0, len(prices) - 1):
+        for j in range(i+1, len(prices) - 1 - i):
+            if prices[i] > prices[j]:
                 length += 1
                 break
-            elif prices[each-1] <= prices[eachAns]:
+            elif prices[i] <= prices[j]:
                 length += 1
-                someL.append(prices[eachAns])
+                someL.append(prices[j])
         answer.append(length)
         length = 0
     print(someL)
