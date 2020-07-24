46405c00e6b4d7043513fdbbea69ee8b35631aab
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-07-24 21:10:01 +0900

    Update stackoqueue.py

--- stackoqueue.py
+++ stackoqueue.py
@@ -1,16 +1,19 @@
 def solution(priorities, location):
     answer = 0
     run = 1
+    nb = []
     while run == 1:
-        if priorities[0] != max(priorities):
-            print(location)
+        if len(priorities) == 0:
+            answer += location
+            run = 0
+        elif priorities[0] != max(priorities):
             priorities.append(priorities.pop(0))
             if location == 0:
                 location += len(priorities)
             else:
                 location -= 1
         else:
+            nb.append(max(priorities))
             priorities.remove(max(priorities))
-            answer += location
     return answer
 print(solution([9, 1, 2, 3, 1, 1, 1, 1], 3))
