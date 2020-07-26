2e77db135f6287c245b160e293ab9ec39d889a03
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-07-26 21:35:22 +0900

    Update another_coding#2.py

--- another_coding#2.py
+++ another_coding#2.py
@@ -1,16 +1,29 @@
 import math
 def quadSol():
-    inp = input().split('+')
-    a = inp[0]
-    b = inp[1]
-    c = inp[2]
-    print(a, b, c)
-    ap = int(a.split('x^2')[0])
-    bp = int(b.split('x')[0])
-    cp = int(c)
-    if ap == '':
-        ap = 1
-    if bp =='':
-        bp = 1
-    print('(x + ', (-bp+(bp**2-4*(ap*cp))**(1/2))/(2*ap), ')(x + ', (-bp-(bp**2-4*(ap*cp))**(1/2))/(2*ap), ')')
+    def root(num, power):
+        return num ** 1/power
+    try:
+        inp = input().split('+')
+        a = inp[0]
+        ap = int(a.split('x^2')[0])
+        if ap == '':
+            ap = 1
+    except:
+        ap = 0
+        pass
+    try:
+        b = inp[1]
+        bp = int(b.split('x')[0])
+        if bp =='':
+            bp = 1
+    except:
+        bp = 0
+        pass
+    try:
+        c = inp[2]
+        cp = int(c)
+        print(a, b, c)
+    except:
+        pass
+    print('(x + ', (-bp + root(bp**2-4*(ap*cp), 2))/(2*ap), ')(x + ', (-bp - root(bp**2-4*(ap*cp), 2))/(2*ap), ')')
 quadSol()
