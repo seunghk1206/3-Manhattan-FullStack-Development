5242861bb58946eb59e05f6b9d3bd46b5d389226
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-07-31 20:58:33 +0900

    Update 위장#4.py

--- 위장#4.py
+++ 위장#4.py
@@ -2,7 +2,6 @@ def solution(clothes):
     answer = 1
     tempL = [x[1] for x in clothes]
     setL = set(tempL)
-    for y in setL:
-        answer *= tempL.count(y)+1
+    for y in setL: answer *= tempL.count(y)+1
     return answer-1
 print(solution([['crow_mask', 'face'], ['blue_sunglasses', 'face'], ['smoky_makeup', 'face']]))
