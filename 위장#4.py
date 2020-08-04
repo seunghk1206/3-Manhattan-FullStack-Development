977848cac8b76210fe2b049ca91ffdb536738183
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-08-04 21:45:58 +0900

    Delete 위장#4.py

--- 위장#4.py
+++
@@ -1,5 +0,0 @@
-def solution(clothes):
-    answer = 1
-    for y in set([x[1] for x in clothes]): answer *= [x[1] for x in clothes].count(y)+1
-    return answer-1
-print(solution([['crow_mask', 'face'], ['blue_sunglasses', 'face'], ['smoky_makeup', 'face']]))
