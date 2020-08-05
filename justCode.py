6609944aad4fc69041ca94af253cc2bfe78e6825
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-08-05 21:37:43 +0900

    Update justCode.py

--- justCode.py
+++ justCode.py
@@ -1,1 +1,9 @@
-print([[each for each in range(10)] for x in range(10)])
+alphabet_string = string.ascii_uppercase
+nmb = [[[eachNum for eachNum in range(10)] for _ in range(10)] for _ in range(10)]
+for x in nmb:
+    for var in x:
+        for each in var:
+            if each == 0:
+                var[var.index(each)] = 'A'
+print(nmb)
+print(alphabet_string)
