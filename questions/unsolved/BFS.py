efb0a67a6b6dc47a03638b84c75f8cb8a9001bd9
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-08-06 13:58:23 +0900

    Create BFS.py

---
+++ questions/unsolved/BFS.py
@@ -0,0 +1,67 @@
+mazeL = []
+coor = []
+cooraround = []
+lenL = []
+path = []
+anonymousNum = 0
+answer = 0
+count = 0
+N, M = map(int, input().split(' '))
+for _ in range(N):
+    Li = str(input())
+    mazeL.append(Li)
+for eachL in range(N):
+    for eachNum in range(M):
+        if mazeL[eachL][eachNum] == '1':
+            coor.append([eachL, eachNum])
+cooraround.append([0, 0])
+coor.remove([0, 0])
+for each in cooraround:
+    path.append(each)
+    if each == [N-1, M-1]:
+        answer += 1
+        lenL.append(answer)
+    else:
+        if [each[0]+1, each[1]] in coor:
+            cooraround.append([each[0]+1, each[1]])
+            coor.remove([each[0]+1, each[1]])
+            count += 1
+        if [each[0], each[1]+1] in coor:
+            cooraround.append([each[0], each[1]+1])
+            coor.remove([each[0], each[1]+1])
+            count += 1
+        if [each[0]-1, each[1]] in coor:
+            cooraround.append([each[0]-1, each[1]])
+            coor.remove([each[0]-1, each[1]])
+            count += 1
+        if [each[0], each[1]-1] in coor:
+            cooraround.append([each[0], each[1]-1])
+            coor.remove([each[0], each[1]-1])
+            count += 1
+    if count > 0:
+        print(count)
+        answer -= count
+        count = 0
+print(min(lenL))
+print(path)
+'''
+1. 우선은 N, M을 인풋으로 받기
+2. for loop 돌려서 2d 리스트 받기
+3. 시작점 [0][0] 지점에서 상하좌우 확인하면서 1이 있으면 answer += 1 그리고 pop(1) + pop한 숫자의 좌표는 다른 리스트에 저장
+4. [N][M]에 도착했을경우 리스트.append(answer)
+5. print(min(리스트))
+BFS
+4 6
+101111
+101010
+101011
+111011
+out: 15
+5 7
+1111111
+1100001
+1110001
+0001001
+1111111
+out: 11
+'''
