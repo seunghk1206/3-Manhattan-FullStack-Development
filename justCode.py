fbf18e74e0c4016f67ad2fdedea5d60a09fd79e5
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-08-06 14:10:18 +0900

    Update justCode.py

--- justCode.py
+++ justCode.py
@@ -1,9 +1,13 @@
+import string
 alphabet_string = string.ascii_uppercase
+##
 nmb = [[[eachNum for eachNum in range(10)] for _ in range(10)] for _ in range(10)]
 for x in nmb:
     for var in x:
         for each in var:
-            if each == 0:
-                var[var.index(each)] = 'A'
+            var[var.index(each)] = alphabet_string[var.index(each)]
 print(nmb)
 print(alphabet_string)
+## is the same as
+nmb_Alpha = [[[eachNum for eachNum in alphabet_string] for _ in range(10)] for _ in range(10)]
+print(nmb_Alpha)
