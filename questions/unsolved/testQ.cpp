2ef1f1531a9f2ded8664b9c1ca3fe016317f0750
Noble-1206 <60473607+Noble-1206@users.noreply.github.com>
2020-09-29 23:52:21 +0900

    Create testQ.cpp

---
+++ questions/unsolved/testQ.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long solution(vector<vector<int> > rectangles)
+{
+    long long answer = -1;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    cout << "Hello Cpp" << endl;
+
+    return answer;
+}
